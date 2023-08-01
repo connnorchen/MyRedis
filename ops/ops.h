d0f5a5f6dcf5a9be968e8792500293a2421a54d2
Shengxian Chen <connor@Shengxians-MacBook-Air.local>
2023-07-31 17:51:39 -0700

    implemented avl-trees

--- ops/ops.h
+++ ops/ops.h
@@ -7,9 +7,6 @@ enum {
     RES_NX = 2,
 };
 
-#define container_of(ptr, type, member) ({                  \
-    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-    (type *)( (char *)__mptr - offsetof(type, member) );})
 
 void do_keys(std::vector<std::string> &cmd, std::string &out);
 void do_get(std::vector<std::string> &cmd, std::string &out);
--- utils/util.h
+++ utils/util.h
@@ -5,6 +5,10 @@ enum {
     ERR_2BIG = 2,
 };
 
+#define container_of(ptr, type, member) ({                  \
+    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+    (type *)( (char *)__mptr - offsetof(type, member) );})
+
 bool cmd_is(const std::string &, const char *);
 void die(const char*, int);
 void msg(const char*, int);
